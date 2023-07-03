52f68de7c6731d8fa988a83f883f4f5d3efafbaa
m0dexl <yahya.altungul@gmail.com>
2023-07-03 18:28:07 +0300

    chess-board

---
+++ src/components/Board/Board.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Board.css";
+import { getCharacter } from "../../helper";
+import Ranks from "../bits/Ranks";
+import Files from "../bits/Files";
+
+const Board = () => {
+  const getClassName = (i, j) => {
+    let c = "tile";
+    c += (i + j) % 2 === 0 ? " tile--dark " : " tile--light ";
+    return c;
+  };
+
+  const ranks = Array(8)
+    .fill()
+    .map((x, i) => 8 - i);
+
+  const files = Array(8)
+    .fill()
+    .map((x, i) => i + 1);
+
+  return (
+    <div className="board">
+      <Ranks ranks={ranks} />
+      <div className="tiles">
+        {ranks.map((rank, i) =>
+          files.map((file, j) => (
+            <div
+              key={file + "-" + rank}
+              className={getClassName(9 - i, j)}
+            ></div>
+          ))
+        )}
+      </div>
+      <Files files={files} />
+    </div>
+  );
+};
+
+export default Board;
---
+++ src/components/bits/Files.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Files.css";
+import { getCharacter } from "../../helper";
+
+const Files = ({ files }) => {
+  return (
+    <div className="files">
+      {files.map((file) => (
+        <span key={file}>{getCharacter(file)}</span>
+      ))}
+    </div>
+  );
+};
+
+export default Files;
